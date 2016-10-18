43d248ee7e0e2f342c9644d159166330f0772ba3
Egor Spirin <Egor Spirin>
2016-10-18 15:29:13 +0300

    changed file

--- app/src/main/java/com/fiit/g131/mafia/TurnActivity.java
+++ app/src/main/java/com/fiit/g131/mafia/TurnActivity.java
@@ -1,54 +1,40 @@
 package com.fiit.g131.mafia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
-import java.util.Vector;
+import java.util.Set;
 
 public class TurnActivity extends AppCompatActivity {
 
-    ListView lv;
-    ArrayList<ArrayList<String> > roles;
-
+    Button cur_role;
+    ArrayList<Set<String>> roles;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_turn);
-        String buf = new String();
-        String[] role_name = getIntent().getStringArrayExtra("roles_names");
-        int[] role_c = getIntent().getIntArrayExtra("roles_c");
-        int kol = getIntent().getExtras().getInt("kol");
-        int cur = getIntent().getExtras().getInt("cur");
-        ArrayList<String> names = getIntent().getStringArrayListExtra("names");
+        setContentView(R.layout.activity_roles);
+        final String[] role_name = getIntent().getStringArrayExtra("roles_names");
+        final int[] role_c = getIntent().getIntArrayExtra("roles_c");
+        final ArrayList<String> names = getIntent().getStringArrayListExtra("names");
         roles = new ArrayList<>();
+        final int kol = getIntent().getExtras().getInt("kol");
+        final int cur = getIntent().getExtras().getInt("cur");
+        cur_role = (Button) findViewById(R.id.cur_role);
+        cur_role.setText(role_name[cur]);
         String key = "a";
         ArrayList<String> tmp;
         for (int i = 0; i < kol; i++) {
             tmp = getIntent().getStringArrayListExtra(key);
-            roles.add(tmp);
+            for (int q = 0; q < tmp.size(); q++) {
+                roles.get(i).add(tmp[q]);
+            }
             key += "a";
         }
-        Vector<String> for_adapter = new Vector<String>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, for_adapter);
-        boolean fl;
-        for (int i = 0; i < names.size(); i++) {
-            fl = true;
-            for (int q = 0; q < roles.get(cur).size(); q++) {
-                if (roles.get(cur).get(q).equals(names.get(i))) {
-                    fl = false;
-                }
-            }
-            fl = true;
-            if (fl) {
-                for_adapter.add(names.get(i));
-            }
-        }
-        lv = (ListView) findViewById(R.id.lv);
-        lv.setAdapter(adapter);
+
     }
 }
